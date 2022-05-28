17439f98179da3bcd7626be4ecfe672364b5d5b8
LSH3333 <dltpgustpgus@gmail.com>
2022-05-28 21:08:02 +0900

    lib

---
+++ Assets/Plugins/_Libs/HRepeat.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace MyEngine
+{
+    // HScroll.cs 를 이용해 Horizontal 방향으로 움직이는 게임오브젝트가 
+    // 화면 밖으로 벗어나면 원위치시켜 반복하도록함 
+	public class HRepeat : MonoBehaviour
+	{
+        private BoxCollider2D _box;
+        private float _horizontalLength;
+
+        public void setBoxCollider()
+        {
+            _box = GetComponent<BoxCollider2D>();
+            _horizontalLength = _box.size.x;
+        }
+
+        public void updateObject()
+        {
+            if (transform.position.x < -_horizontalLength)
+                ResetPosition();
+        }
+
+        private void ResetPosition()
+        {
+            Vector2 addPos = new Vector2(2 * _horizontalLength, 0f);
+            transform.position = (Vector2)transform.position + addPos;
+        }
+
+    }
+}
+
---
+++ Assets/Plugins/_Libs/HScroll.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace MyEngine
+{
+    // 게임오브젝트를 Horizontal 방향으로 움직이도록함 
+	public class HScroll : MonoBehaviour
+	{
+        private Rigidbody2D _rb2d;
+
+        public void setRigidbody(float speed)
+        {
+            _rb2d = GetComponent<Rigidbody2D>();
+            _rb2d.bodyType = RigidbodyType2D.Kinematic;
+            _rb2d.velocity = new Vector2(-speed, 0f);
+        }
+
+        public void setStop()
+        {
+            _rb2d.velocity = Vector2.zero;
+        }
+
+    }
+}
+
+
